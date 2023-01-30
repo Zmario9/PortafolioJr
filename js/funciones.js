b229d0e09989efbf516532f5853ea44bffb99d98
Jose Manuel <jcorreacastro99@gmail.com>
2023-01-29 20:39:36 -0400

    Primer prototipo de estructura CSS completo hecho en SASS de mi portafolio

--- js/funciones.js
+++ js/funciones.js
@@ -1,5 +1,6 @@
 var contador = 1
-const texto = $(".segundoText")
+const texto = $(".segundoText");
+const textoPC =$(".segundoTextoPc");
 const textoCargar =()=>{
     setInterval(()=>{
         console.log(contador);
@@ -7,14 +8,17 @@ const textoCargar =()=>{
         switch(contador){
             case 1: 
                 texto.text("BackEnd Dev");
+                textoPC.text("BackEnd Dev");
                 //console.log("backend"); 
                 break;
             case 2:
                 texto.text("FrontEnd Dev");
+                textoPC.text("FrontEnd Dev");
                 //console.log("FrontEnd");
                 break;
             case 3:
                 texto.text("FullStack Dev");
+                textoPC.text("FullStack Dev");
                 //console.log("Fullstack")
                 contador = 0
                 break;
